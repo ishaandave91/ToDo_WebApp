9798468d451e02f4258744af8f71b011e892b94e
dave.ishaan <ishaanrdave1991@gmail.com>
2023-08-10 15:35:25 +0530

    Handled- duplicates,multiple spaces,no input|Theme updated in streamlit/config.toml

--- web.py
+++ web.py
@@ -1,21 +1,28 @@
+# Today:
+#
+
 import streamlit as st
 import functions
 
+st.set_page_config(layout="wide")
 
 todos = functions.load_file()
 
 
 def add_todo():
-    new_todo = st.session_state['todo_ipbox'] + '\n'
-    if len(new_todo)>0:
-        todos.append(new_todo)
-        todos.sort()
-        functions.write_todos(todos)
-        st.session_state['todo_ipbox'] = ''
+    new_todo = st.session_state['todo_ipbox'].strip()
+    if new_todo not in todos:
+        if len(new_todo) > 0:
+            todos.append(new_todo.capitalize() + '\n')
+            todos.sort()
+            functions.write_todos(todos)
+            st.session_state['todo_ipbox'] = ''
+        else:
+            st.session_state['todo_ipbox'] = ''
     return
 
 
-st.title('Watch List:')
+st.title('Watch List :')
 
 for index, todo_item in enumerate(todos):
     checkbox = st.checkbox(todo_item, key=todo_item)
@@ -26,6 +33,6 @@ for index, todo_item in enumerate(todos):
         st.experimental_rerun()
 
 
-st.text_input(label='', placeholder='Add movie name here...',
+st.text_input(label=' ', placeholder='Add movie name here...',
               on_change=add_todo, key='todo_ipbox')
 st.button('Add', on_click=add_todo, key='add_btn')
